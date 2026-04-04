e8300d98de285171633a068cfd8d6e492624e21f
Nachiket07 <nachiket.nachiket.kelkar@gmail.com>
2026-04-04 17:16:51 -0400

    Ignore env files properly

---
+++ Google/Second trial/second_trial.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv("../local.env")
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-3.1-pro-preview",
+    temperature=1.0,  # Gemini 3.0+ defaults to 1.0
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
---
+++ Ollama/hello world/hello.py
@@ -0,0 +1,19 @@
+from langchain.agents import create_agent
+from langchain_ollama import ChatOllama
+from langchain.tools import tool
+
+@tool
+def sayHello(query: str):
+    """Just returns Hello World no matter the input is
+    """
+    #return "Hello World"
+
+llm = ChatOllama(
+    model="llama3.2",
+    temperature=0,
+)
+
+agent = create_agent(llm, tools = [sayHello])
+
+result = agent.invoke("Hi there!")
+print(result)
